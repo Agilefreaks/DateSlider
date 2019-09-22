4e2484f5f15a45f9af401aced3da74e092d816a7
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2019-09-22 18:41:25 +0300

    Initial project

--- date_slider/lib/main.dart
+++ date_slider/lib/main.dart
@@ -1,62 +1,49 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class MainPage extends StatefulWidget {
+  MainPage({Key key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MainPageState createState() => _MainPageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class _MainPageState extends State<MainPage> {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Container(
+      height: 500,
+      child: Column(children: <Widget>[
+        Container(
+            margin: EdgeInsets.only(bottom: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(8),
+                  bottomRight: Radius.circular(8)),
+              color: Colors.white,
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.23),
+                    offset: Offset(0.0, 6.0),
+                    blurRadius: 3.0),
+                BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.16),
+                  offset: Offset(0.0, 6.0),
+                  blurRadius: 3.0,
+                )
+              ],
+            ),
+            height: 95,
+            width: 400,
+            alignment: Alignment.center,
+            child: DateSlider()),
+      ]),
+    ));
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+  void dispose() {
+    super.dispose();
   }
 }
